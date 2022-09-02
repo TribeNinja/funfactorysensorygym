638fe3d278a4c2fd550c69517d59297b978a3b2a
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2022-09-02 12:44:10 +0600

    constant contant test

--- components/Cta.js
+++ components/Cta.js
@@ -1,8 +1,45 @@
-import MailchimpSubscribe from "react-mailchimp-subscribe";
-import NewsletterForm from "./NewsletterForm";
+// import MailchimpSubscribe from "react-mailchimp-subscribe";
+// import NewsletterForm from "./NewsletterForm";
+
+import axios from "axios";
+import { useState } from "react";
 
 const Cta = () => {
-  const url = process.env.NEXT_PUBLIC_MAILCHIMP_URL;
+  // const url = process.env.NEXT_PUBLIC_MAILCHIMP_URL;
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState(null);
+
+  const handleFormSubmit = () => {
+    setError(null);
+
+    if (!email) {
+      setError("Please enter a valid email address");
+      return null;
+    }
+
+    const url = "https://api.cc.email/v3/contacts/sign_up_form";
+    const body = {
+      email_address: email,
+      list_memberships: ["211717f2-2941-11ed-9bd5-fa163e5bf31a"],
+    };
+
+    axios
+      .post(url, body, {
+        headers: {
+          Authorization:
+            "Bearer " + process.env.NEXT_PUBLIC_CONSTANT_CONTACT_ACCESS_TOKEN,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then((res) => {
+        console.log(res);
+        setError("Subscribed!");
+      });
+  };
 
   return (
     <div className="p-6 md:p-14 mt-16 md:mt-32">
@@ -18,7 +55,7 @@ const Cta = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
           consequat vitae augue eget aliquet. Donec a dui commodo.
         </p> */}
-        <MailchimpSubscribe
+        {/* <MailchimpSubscribe
           url={url}
           render={(props) => {
             const { subscribe, status, message } = props || {};
@@ -30,7 +67,26 @@ const Cta = () => {
               />
             );
           }}
+        /> */}
+        <input
+          onChange={(event) => setEmail(event?.target?.value ?? "")}
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="px-6 py-3 md:px-8 md:py-4 w-full md:w-96 md:rounded-l-lg text-sm md:text-base outline-none"
         />
+        {error && (
+          <div className="font-light text-sm md:text-base max-w-xl text-white">
+            {error}
+          </div>
+        )}
+
+        <button
+          onClick={handleFormSubmit}
+          className="text-white px-6 py-3 md:px-8 md:py-4 bg-ffsgPink rounded-md md:rounded-none md:rounded-r-lg inline-block font-medium text-sm md:text-base"
+        >
+          <p>Subscribe</p>
+        </button>
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import "node_modules/slick-carousel/slick/slick.css";
 import "node_modules/slick-carousel/slick/slick-theme.css";
 import NextNprogress from "nextjs-progressbar";
-import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,8 +9,6 @@ function MyApp({ Component, pageProps }) {
       <NextNprogress color="#FE346E" />
 
       <Component {...pageProps} />
-
-      <MessengerCustomerChat pageId="111131050739842" appId="735141727539744" />
     </>
   );
 }
