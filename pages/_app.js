3592d8cf76fc4c7e46cafcafcc5443b440317571
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2023-01-08 17:54:10 +0600

    added facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,11 @@ import NextNprogress from "nextjs-progressbar";
 
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   const reactAlertOptions = {
     // you can also just use 'bottom center'
     position: positions.BOTTOM_CENTER,
@@ -15,6 +18,19 @@ function MyApp({ Component, pageProps }) {
     // you can also just use 'scale'
     transition: transitions.SCALE,
   };
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID);
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
+
   return (
     <>
       <AlertProvider template={AlertTemplate} {...reactAlertOptions}>
