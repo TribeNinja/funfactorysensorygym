d2f09110cb74be233b4e30ef7b6424687e97a63a
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2023-01-31 13:58:45 +0600

    giftcard + design updates

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,42 +1,40 @@
 const nodemailer = require("nodemailer");
-const fs = require("fs-extra");
+const fs = require("fs");
+const path = require("path");
 
 export default async (req, res) => {
   const { email } = req.body;
 
-  fs.readFile("./html/successOrder.html", "utf8")
-    .then((data) => {
-      let html = data;
+  let html = fs.readFileSync(
+    path.join(__dirname, "../../../../html/newsletter.html"),
+    "utf8"
+  );
 
-      html = html.replace("{{email}}", email);
+  html = html.replace("{{email}}", email);
 
-      let transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        secure: process.env.SECURE_TRANSPORTER, // true for 465, false for other ports
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      });
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    secure: process.env.SECURE_TRANSPORTER, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_SERVER_USER,
+      pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+  });
 
-      let mailOptions = {
-        from: process.env.EMAIL_SERVER_USER, // sender address
-        to: email,
-        bcc: process.env.EMAIL_TO_BE_SENT_TO,
-        subject: "Thank you for subscribing!", // Subject line
-        html: html, // html body
-      };
+  let mailOptions = {
+    from: process.env.EMAIL_SERVER_USER, // sender address
+    to: email,
+    bcc: process.env.EMAIL_TO_BE_SENT_TO,
+    subject: "Thank you for subscribing!", // Subject line
+    html: html, // html body
+  };
 
-      // Send the email
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          return res.status(400).json({ status: "fail" });
-        }
-        res.status(200).json({ status: "success" });
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(400).json({ status: "fail" });
+    }
+    res.status(200).json({ status: "success" });
+  });
 };
