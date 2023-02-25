e8e646c02c1ea06e97f700165f0b2944249d7d43
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2023-02-25 13:43:10 +0600

    connected giftcards to zapier

--- pages/api/orders/giftcard.js
+++ pages/api/orders/giftcard.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 import { config } from "sanity";
 
+async function postToWebhook(webhookURL, data) {
+  try {
+    const response = await axios.post(webhookURL, data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export default async (req, res) => {
   const projectId = config.projectId;
   const dataset = config.dataset;
   const tokenWithWriteAccess = process.env.SANITY_AUTH_TOKEN;
+
+  postToWebhook(process.env.ZAPIER_GIFTCARDS, {
+    ...req.body,
+  });
 
   const { data } = await axios.post(
     `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}?returnIds=true`,
